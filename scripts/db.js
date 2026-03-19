14607cbc9d1fe08c9098463ba54368ac59087286
janse <jansimonboecker40@gmail.com>
2026-03-19 18:21:15 +0100

    add burger images, update db.js with burger dishes

---
+++ scripts/db.js
@@ -0,0 +1,26 @@
+let dishes = [
+    {
+        "category": "Burger & Sandwiches",
+        "name": "Veggie mushroom black burger",
+        "indigredients": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+        "image": "./assets/img/veggie_mushroom_back_burger.svg"
+    },
+    {
+        "category": "Burger & Sandwiches",
+        "name": "All meat burger",
+        "indigredients": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+        "image": "./assets/img/all_meet_burger.svg"
+    },
+    {
+        "category": "Burger & Sandwiches",
+        "name": "Beef red burger",
+        "indigredients": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+        "image": "./assets/img/beef_red_burger.svg"
+    },
+    {
+        "category": "Burger & Sandwiches",
+        "name": "Big chicken burger",
+        "indigredients": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+        "image": "./assets/img/big_chicken_burger.svg"  
+    }
+]
