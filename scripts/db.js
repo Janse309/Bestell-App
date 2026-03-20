13172b6ad6e63f27a5713a4c00729b08ec23cced
janse <jansimonboecker40@gmail.com>
2026-03-20 14:47:38 +0100

    add Pizza array in db.js

--- scripts/db.js
+++ scripts/db.js
@@ -2,35 +2,87 @@ let dishes = [
     {
         "category": "Burger & Sandwiches",
         "name": "Veggie mushroom black burger",
-        "price": "15.90",
+        "price": "16.90",
         "indigredients": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-        "image": "./assets/img/veggie_mushroom_back_burger.svg"
+        "image": "./assets/img/veggie-mushroom-back-burger.jpg"
     },
     {
         "category": "Burger & Sandwiches",
         "name": "All meat burger",
         "price": "15.90",
         "indigredients": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
-        "image": "./assets/img/all_meet_burger.svg"
+        "image": "./assets/img/all-meat-burger.jpg"
     },
     {
         "category": "Burger & Sandwiches",
         "name": "Beef red burger",
+        "price": "14.90",
         "indigredients": "Beef, Cheese, Tomatoes, Lettuce, Onion",
-        "image": "./assets/img/beef_red_burger.svg"
+        "image": "./assets/img/beef-red-burger.jpg"
     },
     {
         "category": "Burger & Sandwiches",
         "name": "Big chicken burger",
         "price": "15.90",
         "indigredients": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
-        "image": "./assets/img/big_chicken_burger.svg"
+        "image": "./assets/img/big-chicken-burger.jpg"
     },
+    // Pizza
     {
         "category": "Pizza",
         "name": "Pizza Margherita",
+        "price": "11.90",
+        "indigredients": "Tomato Sauce, Mozzarella",
+        "image": "./assets/img/pizza-margherita.jpg"
+    },
+    {
+        "category": "Pizza",
+        "name": "Pizza Chorizo",
+        "price": "13.90",
+        "indigredients": "Tomato slices, Mozzarella, Chorizo",
+        "image": "./assets/img/pizza-chorizo.jpg"
+    },
+    {
+        "category": "Pizza",
+        "name": "Funghi",
+        "price": "12.90",
+        "indigredients": "Red onion, Olives, Button Mushrooms, Mozzarella",
+        "image": "./assets/img/funghi.jpg"
+    },
+    {
+        "category": "Pizza",
+        "name": "Quattro Formaggi with Chicken",
         "price": "15.90",
-        "indigredients": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
-        "image": "./assets/img/big_chicken_burger.svg"
-    }
+        "indigredients": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+        "image": "./assets/img/quattro-formaggi-with-chicken.jpg"
+    },
+    // Salat
+    {
+        "category": "Salad",
+        "name": "Warm beef arugula salad",
+        "price": "15.90",
+        "indigredients": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+        "image": "./assets/img/quattro-formaggi-with-chicken.jpg"
+    },
+        {
+        "category": "Salad",
+        "name": "Quattro Formaggi with Chicken",
+        "price": "15.90",
+        "indigredients": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+        "image": "./assets/img/quattro-formaggi-with-chicken.jpg"
+    },
+        {
+        "category": "Salad",
+        "name": "Quattro Formaggi with Chicken",
+        "price": "15.90",
+        "indigredients": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+        "image": "./assets/img/quattro-formaggi-with-chicken.jpg"
+    },
+        {
+        "category": "Salad",
+        "name": "Quattro Formaggi with Chicken",
+        "price": "15.90",
+        "indigredients": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+        "image": "./assets/img/quattro-formaggi-with-chicken.jpg"
+        }
 ]
