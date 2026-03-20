13078ccf3df7228ed19abd8cc40eaf226ee4e9a5
janse <jansimonboecker40@gmail.com>
2026-03-20 05:59:03 +0100

    seperator in index.html added

--- scripts/db.js
+++ scripts/db.js
@@ -2,12 +2,14 @@ let dishes = [
     {
         "category": "Burger & Sandwiches",
         "name": "Veggie mushroom black burger",
+        "price": "15.90",
         "indigredients": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
         "image": "./assets/img/veggie_mushroom_back_burger.svg"
     },
     {
         "category": "Burger & Sandwiches",
         "name": "All meat burger",
+        "price": "15.90",
         "indigredients": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
         "image": "./assets/img/all_meet_burger.svg"
     },
@@ -20,7 +22,15 @@ let dishes = [
     {
         "category": "Burger & Sandwiches",
         "name": "Big chicken burger",
+        "price": "15.90",
         "indigredients": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
-        "image": "./assets/img/big_chicken_burger.svg"  
+        "image": "./assets/img/big_chicken_burger.svg"
+    },
+    {
+        "category": "Pizza",
+        "name": "Pizza Margherita",
+        "price": "15.90",
+        "indigredients": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+        "image": "./assets/img/big_chicken_burger.svg"
     }
 ]
