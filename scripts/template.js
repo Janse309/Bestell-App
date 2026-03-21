3099a97fd74fe8e822576ead9acf08b733c44463
janse <jansimonboecker40@gmail.com>
2026-03-21 16:35:18 +0100

    some stylings added

--- scripts/template.js
+++ scripts/template.js
@@ -2,17 +2,17 @@ function getDishTemplate(index) {
     let dish = dishes[index]
     return `
     <div class="dish-card">
-        <div class="dish-image-container">
-            <img src="${dish.image}" alt="${dishes.name}">
-        </div>
-        <div class="dish-item-container">
+        <div class="meal-container">
+            <div class="dish-image-container">
+                <img src="${dish.image}" alt="${dishes.name}">
+            </div>
             <div class="dish-information-container">
                 <h3 class="dish-headline">${dish.name}</h3>
                 <p class="dish-description">${dish.indigredients}</p>
             </div>
             <div class="dish-price">
                 <p>${dish.price}</p>
-                <button><img src="${dish.button}" alt="Hinzufügen"></button>
+                <button class="basket-btn"><img src="${dish.button}" alt="Hinzufügen"></button> 
             </div>
         </div>
     </div>
