eb1aa16d8d1f2b7ea6696c97b5b8bfb539f9db18
janse <jansimonboecker40@gmail.com>
2026-04-11 17:29:42 +0200

    aria label added and impressum

--- scripts/template.js
+++ scripts/template.js
@@ -1,8 +1,8 @@
 function getDishTemplate(index) {
     let dish = dishes[index];
     return `
-    <div class="dish-cart">
-                <img class="dish-image" src="${dish.image}" alt="${dish.name}">
+        <div class="dish-cart">
+            <img class="dish-image" src="${dish.image}" alt="${dish.name}">
             <div class="dish-information-container">
                 <h3 class="dish-headline">${dish.name}</h3>
                 <p class="dish-description">${dish.indigredients}</p>
@@ -10,32 +10,32 @@ function getDishTemplate(index) {
             <div class="dish-price">
                 <p>${Number(dish.price).toFixed(2).replace('.', ',')}€</p>
                 <div class="mobile-add-btn-container">
-                ${getBasketStatus(dish.name)}
-                <button onclick="addToBasket(${index})" class="add-btn">Add to basket</button>
+                    ${getBasketStatus(dish.name)}
+                    <button aria-label="Zum Warenkorb hinzufügen" onclick="addToBasket(${index})" class="add-btn">Add to basket</button>
                 </div>
             </div>
-    </div>
+        </div>
     `;
 }
 
 function getBasketTemplate(item, index) {
     let totalItemPrice = item.price * item.amount;
     let trashIcon = item.amount === 1
-        ? `<button onclick="deleteItem()" class="trash-icon"><img src="./assets/icons/delete.svg" alt="Löschen"></button>` : `-`;
+        ? `<button aria-label="Gericht aus dem Warenkorb löschen" onclick="deleteItem()" class="trash-icon"><img src="./assets/icons/delete.svg" alt="Löschen"></button>` : `-`;
     let trashClass = item.amount > 1 ? "" : "d_none";
     return `
         <div class="basket-item">
             <div class="basket-item-details">
                 <h3>${item.name}</h3>
-                <button onclick="deleteItem(${index})" class="trash-icon ${trashClass}">
+                <button aria-label="Gericht aus dem Warenkorb löschen" onclick="deleteItem(${index})" class="trash-icon ${trashClass}">
                 <img src="./assets/icons/delete.svg" alt="Löschen">
                 </button> 
             </div>
             <div class="basket-controls">
                 <div class="basket-counter">
-                    <button class="control-btn" onclick="changeAmount(${index}, -1)">${trashIcon}</button>
+                    <button aria-label="Gerichtanzahl um eins veringern" class="control-btn" onclick="changeAmount(${index}, -1)">${trashIcon}</button>
                     <span class="amount-display">${item.amount}</span>
-                    <button class="control-btn" onclick="changeAmount(${index}, 1)">+</button>
+                    <button aria-label="Gerichtanzahl um eins erhöhen" class="control-btn" onclick="changeAmount(${index}, 1)">+</button>
                 </div>
                 <div>${totalItemPrice.toFixed(2).replace('.', ',')} €</div>
                 
